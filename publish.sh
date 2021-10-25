5bbdb3729862e5e260a01b62b744a7a730503183
Borisa Krstic <borisakrstic02@gmail.com>
2021-10-25 16:53:57 +0200

    updated publish shell script

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ echo "Clearing all docker data..."
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
-echo "Pulling changes from git branch - main..."
+echo "Pulling changes from git branch - master..."
 git pull
 echo "Building a docker image..."
 docker build -t like-a-local .
