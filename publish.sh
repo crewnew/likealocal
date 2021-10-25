9f247bc86bc94bb8fad47c6c6d9796a2951ff53b
Borisa Krstic <borisakrstic02@gmail.com>
2021-10-25 16:50:45 +0200

    added publish files

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Clearing all docker data..."
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+echo "Pulling changes from git branch - main..."
+git pull
+echo "Building a docker image..."
+docker build -t like-a-local .
+echo "Running a docker container..."
+docker run -d --restart=always --name like-a-local-fe -p 80:80 like-a-local
